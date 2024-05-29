bc3a328581fb2e52cd15ad425c85872110157d3c
Mukul Agarwal <agarmukul23@gmail.com>
2024-05-29 18:02:31 -0500

    refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +13,9 @@ export default defineConfig({
     shikiConfig: {
       theme: 'catppuccin-latte',
       wrap: true,
-    }
+    },
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex]
   },
   image: {
     domains: ["v5.airtableusercontent.com"]
---
+++ src/lib/date.ts
@@ -0,0 +1,6 @@
+export default function fmtDate(date: Date): string {
+    let year = new Intl.DateTimeFormat("en", { year: "numeric" }).format(date);
+    let month = new Intl.DateTimeFormat("en", { month: "short" }).format(date);
+    let day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(date);
+    return `${day} ${month} ${year}`;
+};
