b3eac19c914be66ae570604d8950e719d303f8e1
Mukul Agarwal <agarmukul23@gmail.com>
2024-06-02 21:52:36 -0500

    add atom:link in website

--- src/pages/posts/rss.xml.js
+++ src/pages/posts/rss.xml.js
@@ -4,8 +4,14 @@ export async function GET(context) {
 	return rss({
 		title: "Mukul's Blog",
 		description: "Mukul Agarwal's Personal Blog",
+		xmlns: {
+			'atom': "http://www.w3.org/2005/Atom"
+		},
 		site: context.site + "/posts/",
 		items: await pagesGlobToRssItems(import.meta.glob("./*.md")),
-		customData: `<language>en-us</language>`,
+		customData: [
+			"<language>en-us</language>",
+			`<atom:link href="${context.site + "posts/rss.xml"}" rel="self" type="application/rss+xml" />`
+		].join("")
 	})
 }
