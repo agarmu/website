bcbc31ba3d0862b458e016d3cf2d20d09cb67717
Mukul Agarwal <agarmukul23@gmail.com>
2025-06-15 16:46:39 -0400

    fix: tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 import rehypeKatex from "rehype-katex"
 import remarkMath from "remark-math"
 import remarkReadingTime from "./src/lib/reading-time"
-import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,9 +16,6 @@ export default defineConfig({
 		sitemap(),
 		mdx(),
 	],
-	vite: {
-		plugins: [tailwindcss()],
-	},
 	markdown: {
 		smartypants: true,
 		shikiConfig: {
