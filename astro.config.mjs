bb0e954c0520729de913d5b922362f9df35101f9
Mukul Agarwal <agarmukul23@gmail.com>
2025-06-15 16:30:11 -0400

    upgrades

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
-import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 import rehypeKatex from "rehype-katex"
 import remarkMath from "remark-math"
